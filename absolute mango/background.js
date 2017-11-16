a45e963b922611e6ffc6f387cc355737375a0079
Raymond Lam <lamycraymond@hotmail.com>
2017-11-15 20:17:16 -0500

    split screenshot into pieces

--- absolute mango/background.js
+++ absolute mango/background.js
@@ -3,7 +3,32 @@ chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.captureVisibleTab(function(img) {
     //console.log(img)
     var image = document.createElement("img");
+    image.onload = split;
     image.src = img;
     document.body.appendChild(image);
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext("2d");
+    var parts = [];
+    //the total # of pieces to split into is pieces^2
+    var pieces = 4;
+
+    function split() {
+      var w10 = image.width / pieces;
+      var h10 = image.height / pieces;
+      canvas.width = w10;
+      canvas.height = h10;
+      for (var i = 0; i < pieces * pieces; i++) {
+        var x = (-w10 * i) % (w10 * pieces);
+        var y = (h10 * i) <= h10 ? 0 : -h10;
+        ctx.drawImage(this, x, y, w10 * pieces, h10 * pieces);
+        parts.push(canvas.toDataURL());
+        //test
+        var slice = document.createElement("img");
+        slice.src = parts[i];
+        document.body.appendChild(slice);
+      }
+      console.log(parts);
+    };
   });
 });
